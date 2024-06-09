4938ff7ddd26d5b36ca71b130bcbdd1aad53646b
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2024-06-09 13:39:19 +0700

    Update RT_Runner.py

--- flakes/RT_Runner.py
+++ flakes/RT_Runner.py
@@ -33,4 +33,56 @@ if __name__ == "__main__":
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed: 0.2f} seconds.")
 
+#! usr/bin/env python3
+# countsync.py
+import asyncio
+
+async def get_some_values_from_io():
+    alfa = [3]
+    alfa.append(2)
+    await asyncio.sleep(1)
+    return alfa
+        
+vals = []
+
+async def fetcher():
+    io_vals = ['a','a']
+    while True:
+        
+        io_vals = await get_some_values_from_io()
+        print('A')
+        for val in io_vals:
+            vals.append(val)
+
+async def monitor():
+    while True:
+        print (len(vals))
+        print(vals)
+
+        await asyncio.sleep(1)
+
+async def main():
+    await asyncio.gather(fetcher(), monitor())
+
+##asyncio.run(main())
+
+async def counter(name: str):
+    for i in range(0, 100):
+        print(f"{name}: {i!s}")
+        await asyncio.sleep(0)
+
+async def main():
+    tasks = []
+    for n in range(0, 4):
+        tasks.append(asyncio.create_task(counter(f"task{n}")))
+
+    while True:
+        tasks = [t for t in tasks if not t.done()]
+        if len(tasks) == 0:
+            return
+
+        await tasks[0]
+
+asyncio.run(main())
+
 
