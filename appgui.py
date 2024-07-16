1fcdd52247652ef22682247a75c9d48f791d8be1
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2024-07-16 17:03:08 +0700

    Update appgui.py

--- appgui.py
+++ appgui.py
@@ -1,31 +1,5 @@
-# Copyright (C) 2024 The Flakes Company Ltd.
-# author: MMR
-
-import sys
-
-from PySide6.QtCore import (QDateTime, QDir, QLibraryInfo, QSysInfo, Qt, QTimer, \
-                            Slot, qVersion)
-from PySide6.QtGui import (QCursor, QDesktopServices, QGuiApplication, QIccn,
-                           QKeySequence, QShortCut, QStandardItem, QStandardItemModel)
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox,
-                               QCommandLinkButton, QDateTimeEdit, QDial,
-                               QDialog, QDialogButtonBox, QFileSystemModel,
-                               QGridLayout, QGroupBox, QHBoxLayout, QLabel,
-                               QLineEdit, QListView, QMenu, QPlainTextEdit,
-                               QPorgressBar, QPushButton, QRadioButton,
-                               QScrollBar, QSizePolicy, QSlider, QSpinBox,
-                               QStyleFactory, QTableWidget, QTabWidget,
-                               QTextBrowser, QTextEdit, QToolBox, QToolButton,
-                               QTreeView, QVBoxLayout, QWiget)
-
-POEM = """ Twinkle, twinkle, little star,
-How I wonder what you are.
-Up above the world so high,
-Like a diamond in the sky.
-Twinkle, twinkle, little star,
-How I wonder what you are."""
-
-DIR_OPEN_ICON = ":/qt-project.org/styles/commonstyle/images/diropen-128.png"
+#====================================================================================#
+# Layout allocation #
 
 import sys
 
@@ -66,8 +40,7 @@ if __name__ == "__main__":
     hlay = QtWidgets.QHBoxLayout(w)
     hlay.addWidget(left_widget, stretch=1)
     hlay.addWidget(right_widget, stretch=3)
-
-    # debug
+  
     bottom_container.setStyleSheet("background-color:salmon;")
     right_widget.setStyleSheet("background-color:gray;")
 
@@ -76,7 +49,8 @@ if __name__ == "__main__":
 
     sys.exit(app.exec())
 
-
+#================================================================================#
+# Plotting layout
 import numpy as np
 import sys
 
@@ -140,13 +114,8 @@ class MW(QWidget):
         layout.addLayout(subLayout3)
 ##        layout.addWidget(PV)
 ##        layout.addWidget(OP)
-        
-
         return result
-        
-        
-
-        
+              
 app = QApplication(sys.argv)
 window = MW()
 window.show()
