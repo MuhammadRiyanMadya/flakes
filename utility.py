c967bf579c4c96b6cbea6f38f28228ac2ae89aa6
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2024-08-27 05:34:20 +0700

    Update utility.py

--- utility.py
+++ utility.py
@@ -4,12 +4,19 @@ import collections
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+import os
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename = r'C:\Users\ssv\Documents\MRM\Exp\UserLog.log',level = logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S', filemode = 'a')
 
 
+        
 class dataExt():
     fl  = flakes.disturbance()
     def __init__(self):
-        pass
+        self.n = 1
     def csvExt(self, path):
         y, _ = fl.modelDFile(path)
         fl.k = np.array([])
@@ -30,13 +37,6 @@ class dataExt():
             return npd
         
     def dataV(self, npd, valveLabel: str):
-##        fig = plt.figure(1)
-##        num = npd.shape[1]
-####        for i in range(1,num):
-####            plt.plot(npd[:,0],npd[:,i])
-##
-##        plt.plot(npd[:,0], npd[:,1])
-##        plt.show()
 
         fig, ax1 = plt.subplots()
 
@@ -64,20 +64,47 @@ class dataExt():
         plt.title(f'{valveLabel} Valve Trend', fontsize = 16, fontweight = 'bold')
         plt.show()
 
-    def largeExt(self,path, fromFolder = False):
-        if fromFolder == False:
-            self.df = pd.read_excel(path, sheet_name = 1)
+    def largeExt(self,path, gradeKey: list, *args, FileErrorFlag = False):
+        
+        for files in os.listdir(path):
+            try:
+                if '.' in files:
+                    filePath = path + '\\' + files
+                    self.df_init = pd.read_excel(filePath)
+                    
+                    for j in gradeKey:
+                        idx = np.where(self.df_init == j)
+                        if len(idx[0]) != 0:
+                            break
+                        
+                    if len(idx[0]) != 0:
+                        self.df = pd.read_excel(filePath, sheet_name = 1)
+                        OP = self.dataBrowser(self.df, '202 B', opIdx = 1)
+                        PV = self.dataBrowser(self.df, '202 B')
+                        PPM = self.dataBrowser(self.df, '201-2', '201 - 2', '201 -2')
 
-            FC201B_PV = self.dataBrowser(self.df, '201 B')
-            FC201B_OP = self.dataBrowser(self.df, '201 B', opIdx = 1)
-            AC201_1 = self.dataBrowser(self.df, '201-1', '201 - 1', '201 -1')
-
-            mydict = {'OP': FC201B_OP, 'PV': FC201B_PV, 'PPM': AC201_1}
-
-            self.dataPool('dataExperiment.xlsx', **mydict)
+                        mydict = {'OP': OP, 'PV': PV, 'PPM': PPM}
+                        self.dataPool(args[0] + '.xlsx', **mydict)
+                        
+                        logging.info(filePath + ':-> INDEX {}'.format(self.n))
+                        self.n += 3
+##                    self.dfGrade = pd.read_excel(filePath)
+##                    grade = self.dataBrowser(self.dfGrade, 'GRADE PROD', searchObj = 'Grade')
+##                    
+##                    mydict = {'OP': grade, 'PV': grade, 'PPM': grade}
+##                    self.dataPool(args[1] + '.xlsx', **mydict)
+                    
+                    
+                elif '.' not in files:
+                    print(self.n)
+                    self.largeExt(path + '\\' + files, gradeKey,*args)
+                else:
+                    FileErrorFlag = True
+            except:
+                print('\n\n "WARNING FILE ERROR" \n {}\{} \n *-------------------* \n'.format(path, files))
+                continue
             
-
-    def dataBrowser(self, file: pd.DataFrame, *args, opIdx = 0):
+    def dataBrowser(self, file: pd.DataFrame, *args, searchObj = 'FC', opIdx = 0):
         dataBlock = np.array([])
         for i in args:
             IdxKey = np.where(file == i)
@@ -87,14 +114,19 @@ class dataExt():
                     print("WARNING: Three identical identifiers are FOUND !")
                 FCrow = IdxKey[0][-1]
                 FCcol = IdxKey[1][-1]
-                for i in range(1,5):
-                    dt = file.iloc[FCrow+i, FCcol+opIdx]
-                    if isinstance(dt, str):
-                        dt = -1
-                    dataBlock = np.append(dataBlock,dt)
-        dataBlock = dataBlock[~np.isnan(dataBlock)]
-##            if len(dataBlock) == 3)
-
+                if searchObj == 'FC':
+                    for z in range(1,5):
+                        dt = file.iloc[FCrow+z, FCcol+opIdx]
+                        if isinstance(dt, str):
+                            dt = -1
+                        dataBlock = np.append(dataBlock,dt)
+                    dataBlock = dataBlock[~np.isnan(dataBlock)]
+                if searchObj == 'Grade':
+                    for z in range(1,5):
+                        dt = file.iloc[FCrow+z, FCcol+opIdx]
+                        if dt == dt:
+                            dataBlock = np.append(dataBlock,dt)
+        
         return dataBlock
 
     def dataPool (self, filename, **kwargs):
@@ -117,18 +149,19 @@ class dataExt():
                             if_sheet_exists = 'overlay'
                             ) as writer:
             datadf_input.to_excel(writer, header = None, index = False, startrow = lastrow + 1)
+        
 
-        
-            
-        
-        
+gradeKey2159 = ['MAS-2159', 'MAS 2159', 'MAS2159', 'MAS~2159']
+gradeKey3355 = ['MAS-3355', 'MAS 3355', 'MAS3355', 'MAS~3355']
+gradeKey3352 = ['MAS-3352', 'MAS 3352', 'MAS3352', 'MAS~3352']
+gradeKey2345 = ['MAS-2345', 'MAS 2345', 'MAS2345', 'MAS~2345']
+
+gradeKey = gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+
+print(gradeKey)
+
 myd = dataExt()
-myd.largeExt(r'C:\Users\mrm\Downloads\MMR\Aptcon\Flakes\largeData Extraction Lab\LST_1\LST~1\01~JAN\03.XLS')
+myd.largeExt(r'D:\Polytama Propindo\Production - Documents\Bulk\Operations\2005\LST\LST~1', gradeKey, 'FC202A')
 
-
-
-##myD = dataExt()
-##
-##npd = myD.excelExt(r"C:\Users\mrm\Downloads\MMR\Aptcon\Flakes\FC202A.xlsx")
-##plots = myD.dataV(npd, 'FC202A')
-
+##npd = myd.excelExt(r"C:\Users\ssv\Documents\MRM\Exp\FC202A.xlsx")
+##plots = myd.dataV(npd, 'FC202A')
