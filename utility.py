7497029d7206906e8b986c0b90923d8f729d1dcf
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2024-08-28 04:21:57 +0700

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
-from flakes import flakes
+import sys
+sys.path.append('../flakes')
+import flakes
 import numpy as np
 import collections
 import matplotlib.pyplot as plt
@@ -7,10 +9,9 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 import os
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename = r'C:\Users\ssv\Documents\MRM\Exp\UserLog.log',level = logging.INFO,
+logging.basicConfig(filename = r'C:\Users\ssv\Documents\MRM\Exp\LargeDataExtractor\UserLogValve_2019.log',level = logging.INFO,
                     format='%(asctime)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S', filemode = 'a')
-
 
         
 class dataExt():
@@ -29,9 +30,12 @@ class dataExt():
         return y,x
     def excelExt(self, path, fromFolder = False):
         if fromFolder == False:
-            df = pd.read_excel(path,header = 0)
+            df = pd.read_excel(path)
             df = df.dropna(thresh =1).dropna(axis=1)
             df = df.sort_values(by=[df.columns.values[0]], ignore_index = True)
+            print(df)
+            df = df[df.iloc[:,2] < 600]
+            print(df)
             npd = df.values
 
             return npd
@@ -44,7 +48,7 @@ class dataExt():
         ax1.set_xlabel('OP, %')
         ax1.set_ylabel('H2 kg/h', color = 'b')
         ax1.tick_params('y', colors='b')
-        ax1.xaxis.set_major_locator(MultipleLocator(1))
+        ax1.xaxis.set_major_locator(MultipleLocator(2))
 ##        ax1.yaxis.set_major_locator(MultipleLocator(0.025))
 
         ax2 = ax1.twinx()
@@ -64,7 +68,7 @@ class dataExt():
         plt.title(f'{valveLabel} Valve Trend', fontsize = 16, fontweight = 'bold')
         plt.show()
 
-    def largeExt(self,path, gradeKey: list, *args, FileErrorFlag = False):
+    def largeExt(self,path, gradeKey: list, *args,FileErrorFlag = False):
         
         for files in os.listdir(path):
             try:
@@ -79,25 +83,32 @@ class dataExt():
                         
                     if len(idx[0]) != 0:
                         self.df = pd.read_excel(filePath, sheet_name = 1)
-                        OP = self.dataBrowser(self.df, '202 B', opIdx = 1)
-                        PV = self.dataBrowser(self.df, '202 B')
-                        PPM = self.dataBrowser(self.df, '201-2', '201 - 2', '201 -2')
+
+##                        OP = self.dataBrowser(self.df, 'A      B', opIdx = 1)
+##                        PV = self.dataBrowser(self.df, 'A      B')
+                        OP = self.dataBrowser(self.df, '201 B', opIdx = 1)
+                        PV = self.dataBrowser(self.df, '201 B')
+                        PPM = self.dataBrowser(self.df, '201-1', '201 - 1', '201 -1')
 
                         mydict = {'OP': OP, 'PV': PV, 'PPM': PPM}
                         self.dataPool(args[0] + '.xlsx', **mydict)
                         
                         logging.info(filePath + ':-> INDEX {}'.format(self.n))
                         self.n += 3
+                        
 ##                    self.dfGrade = pd.read_excel(filePath)
 ##                    grade = self.dataBrowser(self.dfGrade, 'GRADE PROD', searchObj = 'Grade')
 ##                    
 ##                    mydict = {'OP': grade, 'PV': grade, 'PPM': grade}
-##                    self.dataPool(args[1] + '.xlsx', **mydict)
+##                    self.dataPool(args[0] + '.xlsx', **mydict)
+##
+##                    logging.info(filePath + ':-> INDEX {}'.format(self.n))
+##                    self.n += 3
                     
                     
                 elif '.' not in files:
                     print(self.n)
-                    self.largeExt(path + '\\' + files, gradeKey,*args)
+                    self.largeExt(path + '\\' + files, gradeKey, *args)
                 else:
                     FileErrorFlag = True
             except:
@@ -112,8 +123,12 @@ class dataExt():
                 elemNum = len(IdxKey[0])
                 if elemNum > 2:
                     print("WARNING: Three identical identifiers are FOUND !")
+                # General
                 FCrow = IdxKey[0][-1]
                 FCcol = IdxKey[1][-1]
+                # 201 2023,2024
+##                FCrow = IdxKey[0][0]
+##                FCcol = IdxKey[1][0]
                 if searchObj == 'FC':
                     for z in range(1,5):
                         dt = file.iloc[FCrow+z, FCcol+opIdx]
@@ -149,8 +164,10 @@ class dataExt():
                             if_sheet_exists = 'overlay'
                             ) as writer:
             datadf_input.to_excel(writer, header = None, index = False, startrow = lastrow + 1)
-        
 
+gradeKey2161 = ['MAS 2161']
+gradeKey2161 = ['MAS 2162']
+gradeKey2158 = ['MAS-2158', 'MAS 2158', 'MAS2158', 'MAS - 2158']
 gradeKey2159 = ['MAS-2159', 'MAS 2159', 'MAS2159', 'MAS~2159']
 gradeKey3355 = ['MAS-3355', 'MAS 3355', 'MAS3355', 'MAS~3355']
 gradeKey3352 = ['MAS-3352', 'MAS 3352', 'MAS3352', 'MAS~3352']
@@ -158,10 +175,21 @@ gradeKey2345 = ['MAS-2345', 'MAS 2345', 'MAS2345', 'MAS~2345']
 
 gradeKey = gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
 
-print(gradeKey)
+gradeKey_2008 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2010 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2012 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2014 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2016 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2018 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2019 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2020 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2021 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2022 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2023 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
+gradeKey_2024 = gradeKey2158 + gradeKey2161 + gradeKey2159 + gradeKey3355 + gradeKey3352 + gradeKey2345
 
 myd = dataExt()
-myd.largeExt(r'D:\Polytama Propindo\Production - Documents\Bulk\Operations\2005\LST\LST~1', gradeKey, 'FC202A')
+##myd.largeExt(r'D:\Polytama Propindo\Production - Documents\Bulk\Operations\2019\LST-2019\LST~1',gradeKey_2019,'FC201A_2019')
 
-##npd = myd.excelExt(r"C:\Users\ssv\Documents\MRM\Exp\FC202A.xlsx")
-##plots = myd.dataV(npd, 'FC202A')
+npd = myd.excelExt(r"./FC201A_2024.xlsx")
+plots = myd.dataV(npd, 'FC201A_2024')
